917c8e613b6bb4c2e6af2f4fe225b4c399a34fb2
ilyes94 <ilyes.mk94@gmail.com>
2021-04-22 14:00:14 +0200

    Modif css

--- require/css.php
+++ require/css.php
@@ -1,13 +1,4 @@
 <!-- Bootstrap core CSS -->
 <link href="css/bootstrap.css" rel="stylesheet">
-
-<!-- Custom styles for this template -->
 <link href="css/simple-sidebar.css" rel="stylesheet">
-
-<!-- See https://fontawesome.com/v4.7.0/icons/ for more informations -->
-<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        rel="stylesheet"
-        integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-        crossorigin="anonymous"
->
-<link rel="stylesheet" href="css/master.css">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
